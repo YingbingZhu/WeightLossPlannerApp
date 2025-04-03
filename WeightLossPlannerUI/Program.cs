b9ec630c956aa8e0bf35ee5c82537457d960763b
zhuyingb <yingbing.zhu@hotmail.com>
2025-04-03 16:06:50 -0700

    add auth0

--- WeightLossPlannerUI/Program.cs
+++ WeightLossPlannerUI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using WeightLossPlannerUI;
 using Radzen;
 
@@ -10,5 +11,18 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddRadzenComponents();
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.Authority = "https://dev-m50h06zqdmvn5f7l.us.auth0.com/";
+    options.ProviderOptions.ClientId = "EMFkB2r1oFpHAUGjPGrFGW2isFQ8Ygpc";
+    options.ProviderOptions.ResponseType = "code";
+    
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.ProviderOptions.DefaultScopes.Add("email");
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
 
 await builder.Build().RunAsync();
